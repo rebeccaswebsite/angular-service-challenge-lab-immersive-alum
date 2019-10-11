b9501a5b3fbfe7d2af17c5e5a30d8944485b0016
Rebecca Huseyin <rebecca_huseyin@outlook.com>
2019-10-11 08:53:52 +0000

    Automatically backed up by Learn

--- js/app/services/DomainService.js
+++ js/app/services/DomainService.js
@@ -1,5 +1,5 @@
 function DomainService($http) {
-  this.getPosts = function(domainName) {
+  this.getData = function(domainName) {
     return $http.get(`http://localhost:8080/rest/domain/${domainName}`);
   };
 }
